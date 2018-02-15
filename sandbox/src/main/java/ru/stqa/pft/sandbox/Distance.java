05c91da49bcaaa77cdc2416d1dc6378a77081d66
Anastaziya <36408925+Anastaziya@users.noreply.github.com>
2018-02-15 14:42:22 +0300

    окончание 1 модуля

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -0,0 +1,8 @@
+package ru.stqa.pft.sandbox;
+
+import java.awt.*;
+
+public class Distance {
+  public Point p1;
+  public Point p2;
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import java.awt.*;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
@@ -11,10 +13,25 @@ public class MyFirstProgram {
     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(4, 6);
-     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    /*Distance w = new Distance();
+    w.p1.x = 2;
+    w.p1.y = 2;
+    w.p2.x = 3;
+    w.p2.y = 3;
+    // Point p1 = new Point (2, 2);
+    // Point p2 = new Point (3, 3);
+    System.out.println("Расстояние между точками = " + length1(w));
+    */
   }
 
   public static void hello(String somebody) {
      System.out.println("Hello, " + somebody + "!");
   }
+
+ /* public static double length1(Distance d) {
+ //   return Math.sqrt(Math.pow((d.p1.x - d.p2.x), 2) + Math.pow((d.p1.y - d.p2.y), 2));
+  }
+  */
 }
