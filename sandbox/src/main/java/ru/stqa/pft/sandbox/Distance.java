20051a1f043a012a27b596f571984c74d1c41807
Anastaziya <36408925+Anastaziya@users.noreply.github.com>
2018-02-19 16:04:24 +0300

    Сделан первый автотест (создание группы) и оптимизирован код

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -1,8 +1,13 @@
 package ru.stqa.pft.sandbox;
 
-import java.awt.*;
+//import java.awt.*;
 
 public class Distance {
   public Point p1;
   public Point p2;
+
+  public Distance(Point p1, Point p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+  }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -15,23 +15,30 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    /*Distance w = new Distance();
-    w.p1.x = 2;
-    w.p1.y = 2;
-    w.p2.x = 3;
-    w.p2.y = 3;
-    // Point p1 = new Point (2, 2);
-    // Point p2 = new Point (3, 3);
-    System.out.println("Расстояние между точками = " + length1(w));
+    /*Point p1 = new Point (0, 0);
+    Point p2 = new Point (3, 3);
+    System.out.println("Расстояние между точками = " + length1(p1, p2));
     */
+
+    Point p1 = new Point(1 , 1);
+    Point p2 = new Point(3 , 3);
+    Distance d1 = new Distance(p1, p2);
+    Distance d = new Distance((1, 1), (2, 2));
+    System.out.println("Расстояние между точками = " + length1(d));
+
   }
 
   public static void hello(String somebody) {
      System.out.println("Hello, " + somebody + "!");
   }
 
- /* public static double length1(Distance d) {
- //   return Math.sqrt(Math.pow((d.p1.x - d.p2.x), 2) + Math.pow((d.p1.y - d.p2.y), 2));
+  /* public static double length1(Point p1, Point p2) {
+     return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
   }
   */
+
+  public static double length1(Distance d) {
+    return Math.sqrt(Math.pow((d.p1.x - d.p2.x), 2) + Math.pow((d.p1.y - d.p2.y), 2));
+  }
+
 }
