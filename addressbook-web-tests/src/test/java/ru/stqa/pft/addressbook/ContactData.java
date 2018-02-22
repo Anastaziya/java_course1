591348be52bcadd9e8e230b4b4b8115c13e50015
Anastaziya <36408925+Anastaziya@users.noreply.github.com>
2018-02-22 11:09:56 +0300

    Проведён рефакторинг теста Создания Нового контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String companyname;
+  private final String phonenumber;
+
+  public ContactData(String firstname, String lastname, String companyname, String phonenumber) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.companyname = companyname;
+    this.phonenumber = phonenumber;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getCompanyname() {
+    return companyname;
+  }
+
+  public String getPhonenumber() {
+    return phonenumber;
+  }
+}
